3b67524e53eab9e1ab03f5906d23f6db471dfae3
Jayjeet1013 <jayjeet1013@gmail.com>
2025-04-09 01:24:32 +0530

    update

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -34,6 +34,7 @@ const Index: React.FC = () => {
               >
                 Blog
               </a>
+              
               <a
                 href="/about"
                 className="ml-4 text-gray-500 hover:text-gray-700 text-sm"
